dd6f6a00c426255834e79190c334b9e57c8b7d24
corpsedbooz4321 <mahiadit4321@gmail.com>
2026-09-14 23:43:46 +0530

    feat: add CORS middleware to enable cross-origin requests

--- app/main.py
+++ app/main.py
@@ -1,9 +1,18 @@
 # main.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.routes import router as student_router
 
 app = FastAPI(title="Mark Registry Web API")
 
 """connect router to main app instance"""
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # allowing all dev versions
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(student_router, prefix="/api")
