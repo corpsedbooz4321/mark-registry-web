3c571a7c898486cd85fd3653eefa0541fc05ea38
corpsedbooz4321 <mahiadit4321@gmail.com>
2026-09-14 22:57:28 +0530

    fix: add prefix to student router in main application

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+# main.py
 from fastapi import FastAPI
 
 from app.api.routes import router as student_router
@@ -5,4 +6,4 @@ from app.api.routes import router as student_router
 app = FastAPI(title="Mark Registry Web API")
 
 """connect router to main app instance"""
-app.include_router(student_router)
+app.include_router(student_router, prefix="/api")
