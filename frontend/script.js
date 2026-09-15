f690e908aca77277dd2b905699a20d2f22655c9f
corpsedbooz4321 <mahiadit4321@gmail.com>
2026-09-16 00:07:50 +0530

    feat: wired the html to script.js for the marks updating feature

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,58 @@
 const API = "http://127.0.0.1:8000/api";
 
+
+async function home() {
+  try {
+    const response = await fetch(`${API}/`)
+
+    const data = await response.json();
+    console.log(data.message);
+    alert(data.message);
+  } catch (error) {
+    console.error("could not connect to the backend:", error);
+  }
+}
+
+async function add_or_update_mark() {
+  const studentId = document.getElementById("student-id").value;
+  const subject = document.getElementById("subject").value;
+  const marks = Number(document.getElementById("marks").value);
+
+  if (!studentId || !subject || isNaN(marks)) {
+    alert("Pleach fill in all the fields!!")
+    return;
+  }
+
+  const payload = {
+    subject: subject,
+    score: marks
+  };
+
+  try {
+    const response = await fetch(`${API}/students/${studentId}/marks`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(payload)
+
+    });
+
+    if (!response.ok) {
+      throw new Error(errorData.detail || `${response.status}`);
+    }
+    const data = await response.json();
+    alert("Marks has been updated!")
+    console.log("Updated dat:", data);
+
+    document.getElementById("subject").value = "";
+    document.getElementById("marks").value = "";
+  } catch (error) {
+    console.error("Failed to update marks:", error);
+    alert("Could not update marks. Check console for details");
+  }
+}
+
 async function load_students() {
   const res = await fetch(`${API}/students/`);
   const students = await res.json();
