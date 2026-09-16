d659a92d33943aab9e64e5c99ee14ba3ab13e6f6
corpsedbooz4321 <mahiadit4321@gmail.com>
2026-09-17 01:17:13 +0530

    refactor: standardize parameter names in student-related API routes

--- app/api/routes.py
+++ app/api/routes.py
@@ -8,16 +8,19 @@ from app.storage.save_students import save_students
 router = APIRouter()
 
 
+# home
 @router.get("/")
 def home():
     return {"message": "welcome to the Mark Registry Web API"}
 
 
+# shows the list of whole student data in (rollno. - name) format
 @router.get("/students/", response_model=list[Student])
 def get_all_students():
     return load_students()
 
 
+# api to create a new student
 @router.post("/students/", response_model=Student)
 def create_students(student: Student):
     students = load_students()
@@ -29,11 +32,12 @@ def create_students(student: Student):
     return student
 
 
-@router.put("/students/{student_id}/marks", response_model=Student)
-def add_or_update_mark(student_id: int, mark: MarkEntry):
+# api to update or add the mark and subject
+@router.put("/students/{id}/marks", response_model=Student)
+def add_or_update_mark(id: int, mark: MarkEntry):
     students = load_students()
     for student in students:
-        if student.id == student_id:
+        if student.id == id:
             for existing_mark in student.marks:
                 if (
                     existing_mark.subject.strip().lower()
@@ -48,19 +52,18 @@ def add_or_update_mark(student_id: int, mark: MarkEntry):
     raise HTTPException(status_code=404, detail="Student not found!")
 
 
-@router.delete("/students/{student_id}")
-def delete_student(student_id: int):
+# api to delete or remove and student by using their roll number
+@router.delete("/students/{id}")
+def delete_student(id: int):
     # load the existing student list from json
     students = load_students()
 
-    target_student = next((s for s in students if s.id == student_id), None)
+    target_student = next((s for s in students if s.id == id), None)
     if not target_student:
         raise HTTPException(status_code=404, detail="Student not found!!")
 
-    updated_students = [s for s in students if s.id != student_id]
+    updated_students = [s for s in students if s.id != id]
 
     save_students(updated_students)
 
-    return {
-        "message": f"Student'{target_student.name} (ID: {student_id} removed successfully"
-    }
+    return {"message": f"Student'{target_student.name} (ID: {id} removed successfully"}
